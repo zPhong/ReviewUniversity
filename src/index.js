fb64fa7cf9b5070c9f12be3fd53fd4988892cd18
Le Vo Gia Khang <levogiakhang@gmail.com>
2019-03-16 18:28:37 +0700

    Add basic router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/index.css';
-import Home from './screens/Home/Home';
-import * as serviceWorker from './serviceWorker';
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
+import Home from './screens/Home/Home'
+import UniversityDetailPage from './screens/UniversityDetail/js/UniversityDetailPage'
+import Notfound from './screens/notfound'
 
-ReactDOM.render(<Home />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          {/*
+            within exact property the router will render only the URL is visited
+          */}
+          <NavLink exact activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/university">
+            UniversityDetailPage
+          </NavLink>
+        </li>
+      </ul>
+      <hr />
+      {/*
+      The Switch component will work much in the same way as the Router component
+      The added functionality of Switch is that it will only render the first matched <Route/> child
+      The Switch will render exactly URL matched.
+      Router components render inclusively of all route matches.
+      */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/university" component={UniversityDetailPage} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
+
