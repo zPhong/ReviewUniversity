1f300a61199643660679a07748b0154851cfe1ac
Lục Trường Phong <luctruongphong13@gmail.com>
2019-04-01 06:29:01 +0700

    Updates

---
+++ precache-manifest.97442acac19243848c0c48b25a552ebc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReviewUniversity/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b494a9f240cf515ed596848ff2508311",
+    "url": "/ReviewUniversity/static/media/loading.b494a9f2.png"
+  },
+  {
+    "revision": "3572117d8b4382099cbd",
+    "url": "/ReviewUniversity/static/js/runtime~main.3572117d.js"
+  },
+  {
+    "revision": "600958e9076c461fef72",
+    "url": "/ReviewUniversity/static/js/main.600958e9.chunk.js"
+  },
+  {
+    "revision": "cb7498fd6cc4eb5e1061",
+    "url": "/ReviewUniversity/static/js/2.cb7498fd.chunk.js"
+  },
+  {
+    "revision": "600958e9076c461fef72",
+    "url": "/ReviewUniversity/static/css/main.af7e012f.chunk.css"
+  },
+  {
+    "revision": "cb7498fd6cc4eb5e1061",
+    "url": "/ReviewUniversity/static/css/2.893b7da7.chunk.css"
+  },
+  {
+    "revision": "160b279cffce0440b48c4048d069b2fe",
+    "url": "/ReviewUniversity/index.html"
+  }
+];
