bcafc315460e48b89891819d5a459c3add5714ef
Le Vo Gia Khang <levogiakhang@gmail.com>
2019-03-31 17:09:01 +0700

    Fix PostDialog UI

--- src/screens/UniversityDetail/components/Dialog/PostDialog.js
+++ src/screens/UniversityDetail/components/Dialog/PostDialog.js
@@ -1,54 +1,54 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-alert */
 /* eslint-disable react/no-find-dom-node */
-import * as React from 'react';
-import $ from 'jquery';
-import ReactDOM from 'react-dom';
-import Recaptcha from 'react-recaptcha';
-import './css/PostDialog.css';
-import APIModel from '../../../../api/APIModel';
+import * as React from "react";
+import $ from "jquery";
+import ReactDOM from "react-dom";
+import Recaptcha from "react-recaptcha";
+import "./css/PostDialog.css";
+import APIModel from "../../../../api/APIModel";
 
-const captchaKeyApi = '6LeR_JkUAAAAAGErmsKDv410V24gf5bHBHfeyfug';
+const captchaKeyApi = "6LeR_JkUAAAAAGErmsKDv410V24gf5bHBHfeyfug";
 
 const Roles = {
-  student: 'Học sinh',
-  employee: 'Cán bộ trường',
-  others: 'Người ngoài'
+  student: "Học sinh",
+  employee: "Cán bộ trường",
+  others: "Người ngoài"
 };
 
 const Types = {
   Review: {
-    like: 'Khen',
-    dislike: 'Chê',
-    others: 'Góp ý'
+    like: "Khen",
+    dislike: "Chê",
+    others: "Góp ý"
   },
   Reply: {
-    like: 'Tán thành',
-    dislike: 'Phản bác',
-    others: 'Trung lập'
+    like: "Tán thành",
+    dislike: "Phản bác",
+    others: "Trung lập"
   }
 };
 
 const dialogDisplayType = {
-  Review: 'Nhận Xét',
-  Reply: 'Trả lời'
+  Review: "Nhận Xét",
+  Reply: "Trả lời"
 };
 
 class ReviewPostDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      role: 'others',
-      type: 'others',
-      content: '',
+      role: "others",
+      type: "others",
+      content: "",
       captchaChecked: false
     };
   }
 
   componentDidMount() {
     const { onClose } = this.props;
-    $(ReactDOM.findDOMNode(this)).modal('show');
-    $(ReactDOM.findDOMNode(this)).on('hidden.bs.modal', onClose);
+    $(ReactDOM.findDOMNode(this)).modal("show");
+    $(ReactDOM.findDOMNode(this)).on("hidden.bs.modal", onClose);
   }
 
   callback = () => {
@@ -59,7 +59,7 @@ class ReviewPostDialog extends React.Component {
     const { role, type, content, captchaChecked } = this.state;
     const { universityId, reviewId, dialogType } = this.props;
 
-    const id = dialogType === 'Review' ? { universityId } : { reviewId };
+    const id = dialogType === "Review" ? { universityId } : { reviewId };
 
     const data = {
       ...id,
@@ -68,22 +68,19 @@ class ReviewPostDialog extends React.Component {
       context: content
     };
     if (!captchaChecked || content.length < 15) {
-      alert('Vui lòng kiểm tra lại nội dung nhập có đủ tối thiểu 15 chữ hay chưa');
+      alert("Vui lòng kiểm tra lại nội dung nhập có đủ tối thiểu 15 chữ hay chưa");
       return;
     }
 
     let result;
-    if (dialogType === 'Review')
-    {
-      result =  await APIModel.postReview(data);
-    }
-    else result = await APIModel.postReply(data);
+    if (dialogType === "Review") {
+      result = await APIModel.postReview(data);
+    } else result = await APIModel.postReply(data);
 
-    if(result !== true)
-    {
-       alert(result);
+    if (result !== true) {
+      alert(result);
     }
-    document.getElementById('btnClose').click();
+    document.getElementById("btnClose").click();
 
   };
 
@@ -91,98 +88,98 @@ class ReviewPostDialog extends React.Component {
     const { dialogType } = this.props;
     const { role, type } = this.state;
     return (
-      <form>
-        <div className="form-group">
-          <p>Vai trò</p>
-          <div className="CustomDropDown">
-            <select
-              className="dropdown-toggle btn-secondary btn"
-              value={role}
+       <form className="m-0">
+         <div className="form-group row m-0 p-0">
+           <h4 className="col-12 col-lg-3 align-self-center">Vai trò</h4>
+           <div className="col-12 col-lg-9 CustomDropDown">
+             <select
+                className="dropdown-toggle form-control"
+                value={role}
+                onChange={(e) => {
+                  this.setState({ role: e.target.value });
+                }}>
+               <p> {role || "Vai trò của bạn"}</p>
+               {Object.keys(Roles).map((key) => (
+                  <option className="dropdown-item" value={key}>
+                    {Roles[key]}
+                  </option>
+               ))}
+             </select>
+           </div>
+         </div>
+
+         <div className="form-group row m-0 p-0 mt-2">
+           <h4 className="col-12 col-lg-3 align-self-center">Đánh giá</h4>
+           <div className="col-12 col-lg-9 CustomDropDown">
+             <select
+                className="dropdown-toggle form-control"
+                value={type}
+                onChange={(e) => {
+                  this.setState({ type: e.target.value });
+                }}>
+               <p> {role || "Đánh giá của bạn"}</p>
+               {Object.keys(Types[dialogType]).map((key) => (
+                  <option className="dropdown-item" value={key}>
+                    {Types[dialogType][key]}
+                  </option>
+               ))}
+             </select>
+           </div>
+         </div>
+
+         <div className="dropdown-divider"/>
+
+         <div className="form-group">
+           <h4 className="col-12 align-self-center">Nội dung (*)</h4>
+           <textarea
+              placeholder="Nhận xét đi..."
+              rows={5}
+              required
               onChange={(e) => {
-                this.setState({ role: e.target.value });
+                this.setState({ content: e.target.value });
               }}
-            >
-              <p> {role || 'Vai trò của bạn'}</p>
-              {Object.keys(Roles).map((key) => (
-                <option className="dropdown-item" value={key}>
-                  {Roles[key]}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <div className="form-group">
-          <p>Đánh giá</p>
-          <div className="CustomDropDown">
-            <select
-              className="dropdown-toggle btn-secondary btn"
-              value={type}
-              onChange={(e) => {
-                this.setState({ type: e.target.value });
-              }}
-            >
-              <p> {role || 'Đánh giá của bạn'}</p>
-              {Object.keys(Types[dialogType]).map((key) => (
-                <option className="dropdown-item" value={key}>
-                  {Types[dialogType][key]}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <div className="form-group">
-          <p>Nội dung (*)</p>
-          <textarea
-            type="text"
-            className="w-100"
-            placeholder="Nhận xét đi..."
-            rows={5}
-            required
-            onChange={(e) => {
-              this.setState({ content: e.target.value });
-            }}
-          />
-        </div>
-      </form>
+           />
+         </div>
+       </form>
     );
   };
 
   render() {
     const { onClose, dialogType, context } = this.props;
     return (
-      <div className="modal fade">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              {context && (
-                <div className="alert alert-info contentReview" role="alert">
-                  {context}
-                </div>
-              )}
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={onClose}>
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              {this.renderContent()}
-              <Recaptcha
-                ref={(e) => (this.recaptchaInstance = e)}
-                size="normal"
-                sitekey={captchaKeyApi}
-                verifyCallback={this.callback}
-              />
-            </div>
-            <div className="modal-footer">
-              <button id="btnClose" type="button" className="btn btn-secondary" data-dismiss="modal" onClick={onClose}>
-                Đóng
-              </button>
-              <button type="button" className="btn btn-primary" onClick={this.onSubmit}>
-                {dialogDisplayType[dialogType] || 'Đăng'}
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+       <div className="modal fade">
+         <div className="modal-dialog">
+           <div className="modal-content">
+             <div className="modal-header">
+               {context && (
+                  <div className="alert alert-info contentReview" role="alert">
+                    {context}
+                  </div>
+               )}
+               <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={onClose}>
+                 <span aria-hidden="true">&times;</span>
+               </button>
+             </div>
+             <div className="modal-body">
+               {this.renderContent()}
+               <Recaptcha
+                  ref={(e) => (this.recaptchaInstance = e)}
+                  size="normal"
+                  sitekey={captchaKeyApi}
+                  verifyCallback={this.callback}
+               />
+             </div>
+             <div className="modal-footer">
+               <button id="btnClose" type="button" className="close-btn" data-dismiss="modal" onClick={onClose}>
+                 Đóng
+               </button>
+               <button type="button" className="submit-btn" onClick={this.onSubmit}>
+                 {dialogDisplayType[dialogType] || "Đăng"}
+               </button>
+             </div>
+           </div>
+         </div>
+       </div>
     );
   }
 }
